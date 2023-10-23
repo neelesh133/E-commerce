0e8cb181c4c2e7bdf5ea3ade33af0ecee5fa56e4
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-10-23 17:48:50 +0530

    Navbar responsiveness added

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,24 @@
 "use client";
 
+import { GlobalContext } from "@/context";
 import { adminNavOptions, navOptions, styles } from "@/utils";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
+import CommonModal from "../CommonModal";
 
-const isAdminView = true;
+const isAdminView = false;
 const isAuthUser = true;
 const user = {
   role: "admin",
 };
 
-function NavItems() {
+function NavItems({ isModalView = false }) {
+
   return (
     <div
-      className="items-center justify-bertween w-full md:flex md:w-auto"
+      className={`items-center justify-bertween w-full md:flex md:w-auto ${isModalView ? "" : 'hidden'}`}
       id="nav-items"
     >
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white">
+      <ul className={`flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 bg-white ${isModalView ? "border-none" : "border-gray-100"}`}>
         {isAdminView
           ? adminNavOptions.map((item) => (
               <li
@@ -39,6 +42,7 @@ function NavItems() {
 }
 
 export default function Navbar() {
+  const { showNavModal, setShowNavModal } = useContext(GlobalContext);
   return (
     <>
       <nav className="bg-white fixed w-full z-20 top-0 left-0 border-b border-gray-200">
@@ -73,6 +77,9 @@ export default function Navbar() {
               className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
               aria-controls="navbar-sticky"
               aria-expanded="false"
+              onClick={() => {
+                setShowNavModal(true);
+              }}
             >
               <span className="sr-only">Open main menu</span>
               <svg
@@ -90,9 +97,15 @@ export default function Navbar() {
               </svg>
             </button>
           </div>
-          <NavItems />
+          <NavItems isModal={false} />
         </div>
       </nav>
+      <CommonModal
+        showModalTitle={false}
+        mainContent={<NavItems isModalView={true} />}
+        show={showNavModal}
+        setShow={setShowNavModal}
+      />
     </>
   );
 }
--- src/context/index.js
+++ src/context/index.js
@@ -1,12 +1,14 @@
-"use client"
+"use client";
 
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const GlobalContext = createContext(null);
 
-
-export default function GlobalState({children}){
-    return(
-        <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>
-    )
+export default function GlobalState({ children }) {
+  const [showNavModal, setShowNavModal] = useState(false);
+  return (
+    <GlobalContext.Provider value={{ showNavModal, setShowNavModal }}>
+      {children}
+    </GlobalContext.Provider>
+  );
 }
