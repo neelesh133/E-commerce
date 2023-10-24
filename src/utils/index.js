a5bc5112269cdb48789267339d7ba8101f5ac143
Neelesh Saxena <saxenaneelo133@gmail.com>
2023-10-24 17:01:48 +0530

    Login form completed

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,81 +1,98 @@
 export const navOptions = [
-    {
-      id: "home",
-      label: "Home",
-      path: "/",
-    },
-    {
-      id: "listing",
-      label: "All Products",
-      path: "/product/listing/all-products",
-    },
-    {
-      id: "listingMen",
-      label: "Men",
-      path: "/product/listing/men",
-    },
-    {
-      id: "listingWomen",
-      label: "Women",
-      path: "/product/listing/women",
-    },
-    {
-      id: "listingKids",
-      label: "Kids",
-      path: "/product/listing/kids",
-    },
-  ];
-  
-  export const adminNavOptions = [
-    {
-      id: "adminListing",
-      label: "Manage All Products",
-      path: "/admin-view/all-products",
-    },
-    {
-      id: "adminNewProduct",
-      label: "Add New Product",
-      path: "/admin-view/add-product",
-    },
-  ]; 
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "listing",
+    label: "All Products",
+    path: "/product/listing/all-products",
+  },
+  {
+    id: "listingMen",
+    label: "Men",
+    path: "/product/listing/men",
+  },
+  {
+    id: "listingWomen",
+    label: "Women",
+    path: "/product/listing/women",
+  },
+  {
+    id: "listingKids",
+    label: "Kids",
+    path: "/product/listing/kids",
+  },
+];
 
-  export const registrationFormControls = [
-    {
-      id: 'name',
-      type: 'text',
-      placeholder: 'Enter your name',
-      label: "Name",
-      componentType: 'input'
-    },
-    {
-      id: 'email',
-      type: 'email',
-      placeholder: 'Enter your email',
-      label: "Email",
-      componentType: 'input'
-    },
-    {
-      id: 'password',
-      type: 'password',
-      placeholder: 'Enter your paswword',
-      label: "Password",
-      componentType: 'input'
-    },
-    {
-      id: 'role',
-      type: '',
-      placeholder: '',
-      label: "Role",
-      componentType: 'select',
-      options: [
-        {
-          id: 'admin',
-          label: 'Admin'
-        },
-        {
-          id: 'customer',
-          label: 'Customer'
-        },
-      ]
-    },
-  ]
+export const adminNavOptions = [
+  {
+    id: "adminListing",
+    label: "Manage All Products",
+    path: "/admin-view/all-products",
+  },
+  {
+    id: "adminNewProduct",
+    label: "Add New Product",
+    path: "/admin-view/add-product",
+  },
+];
+
+export const registrationFormControls = [
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter your name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your paswword",
+    label: "Password",
+    componentType: "input",
+  },
+  {
+    id: "role",
+    type: "",
+    placeholder: "",
+    label: "Role",
+    componentType: "select",
+    options: [
+      {
+        id: "admin",
+        label: "Admin",
+      },
+      {
+        id: "customer",
+        label: "Customer",
+      },
+    ],
+  },
+];
+
+export const loginFormControls = [
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+];
